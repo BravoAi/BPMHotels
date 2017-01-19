968c356bb2e75dd18e60c9d57447983b6a602526
yonatan <>
2017-01-19 02:26:48 -0500

    {/HotelManagement/src/main/java/com/bravo/management/jobCategoryObject.java}

--- HotelManagement/src/main/java/com/bravo/management/jobCategoryObject.java
+++ HotelManagement/src/main/java/com/bravo/management/jobCategoryObject.java
@@ -5,28 +5,60 @@ package com.bravo.management;
  */
 
 @javax.persistence.Entity
-public class jobCategoryObject implements java.io.Serializable {
+public class jobCategoryObject implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "JOBCATEGORYOBJECT_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "JOBCATEGORYOBJECT_ID_SEQ", name = "JOBCATEGORYOBJECT_ID_GENERATOR")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "JOBCATEGORYOBJECT_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(sequenceName = "JOBCATEGORYOBJECT_ID_SEQ", name = "JOBCATEGORYOBJECT_ID_GENERATOR")
+   private java.lang.Long id;
 
-    public jobCategoryObject() {
-    }
-    
-    public jobCategoryObject(java.lang.Long id) {
-        this.id = id;
-    }
+   private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   private java.lang.String iconURL;
+
+   public jobCategoryObject()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.lang.String getName()
+   {
+      return this.name;
+   }
+
+   public void setName(java.lang.String name)
+   {
+      this.name = name;
+   }
+
+   public java.lang.String getIconURL()
+   {
+      return this.iconURL;
+   }
+
+   public void setIconURL(java.lang.String iconURL)
+   {
+      this.iconURL = iconURL;
+   }
+
+   public jobCategoryObject(java.lang.Long id, java.lang.String name,
+         java.lang.String iconURL)
+   {
+      this.id = id;
+      this.name = name;
+      this.iconURL = iconURL;
+   }
 
 }
