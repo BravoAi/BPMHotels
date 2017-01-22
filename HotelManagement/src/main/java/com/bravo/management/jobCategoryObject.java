637f2f134c7a34065f4792f75ab24ce4e5ddeb3c
yonatan <>
2017-01-22 02:02:43 -0500

    {/HotelManagement/src/main/java/com/bravo/management/jobCategoryObject.java}

--- HotelManagement/src/main/java/com/bravo/management/jobCategoryObject.java
+++ HotelManagement/src/main/java/com/bravo/management/jobCategoryObject.java
@@ -10,27 +10,12 @@ public class jobCategoryObject implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "JOBCATEGORYOBJECT_ID_GENERATOR")
-   @javax.persistence.Id
-   @javax.persistence.SequenceGenerator(sequenceName = "JOBCATEGORYOBJECT_ID_SEQ", name = "JOBCATEGORYOBJECT_ID_GENERATOR")
-   private java.lang.Long id;
-
    private java.lang.String name;
 
    private java.lang.String iconURL;
 
    public jobCategoryObject()
    {
-   }
-
-   public java.lang.Long getId()
-   {
-      return this.id;
-   }
-
-   public void setId(java.lang.Long id)
-   {
-      this.id = id;
    }
 
    public java.lang.String getName()
@@ -53,18 +38,17 @@ public class jobCategoryObject implements java.io.Serializable
       this.iconURL = iconURL;
    }
 
-   public jobCategoryObject(java.lang.Long id, java.lang.String name,
-         java.lang.String iconURL)
+   public String toString()
    {
-      this.id = id;
+      String str = "id: " + id;
+      str += "  name: " + name;
+      str += "  iconURL: " + iconURL;
+      return str;
+   }
+
+   public jobCategoryObject(java.lang.String name, java.lang.String iconURL)
+   {
       this.name = name;
       this.iconURL = iconURL;
-   }
-    
-    public String toString(){
-       String str="id: "+id;
-       str+="  name: "+ name;
-      str+="  iconURL: "+ iconURL;
-      return str;
    }
 }
